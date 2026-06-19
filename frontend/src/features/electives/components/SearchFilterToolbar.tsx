f7e593ccff267f34523e38674c1463931c0b3ba4
kostya2505 <kostya250506@gmail.com>
2026-06-19 16:22:16 +0800

    feat(frontend): add search and filter in admin electives page

--- frontend/src/features/electives/components/SearchFilterToolbar.tsx
+++ frontend/src/features/electives/components/SearchFilterToolbar.tsx
@@ -46,7 +46,7 @@ export const SearchFilterToolbar = () => {
   };
 
   return (
-    <div className="flex items-center gap-4 mb-6">
+    <div className="flex items-center gap-4 mb-3">
       {/* Search input – always visible, grows to fill space */}
       <div className="relative flex-1">
         <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
--- frontend/src/pages/AdminElectivesPage.tsx
+++ frontend/src/pages/AdminElectivesPage.tsx
@@ -1,6 +1,8 @@
 import { useAuth } from '@/shared/hooks/useAuth';
 import { ElectiveList } from '@/features/electives/components/ElectiveList';
 import { Navigate } from 'react-router-dom';
+import {SearchFilterToolbar} from "@/features/electives/components/SearchFilterToolbar";
+import {useFilteredElectives} from "@/features/electives/hooks/useFilteredElectives";
 
 export function AdminElectivesPage() {
   const { session, isAdminMode } = useAuth();
@@ -12,10 +14,13 @@ export function AdminElectivesPage() {
 
   const electives = session?.allElectives ?? [];
 
+  const filteredElectives = useFilteredElectives(electives);
+
   return (
     <div className="min-h-screen text-base font-medium  bg-gray-100 dark:bg-gray-900 text-gray-700 dark:text-gray-300">
       <h1>Admin Electives Page</h1>
-      <ElectiveList electives={electives} isFavorite={() => false} onToggleFavorite={() => {}} />
+      <SearchFilterToolbar />
+      <ElectiveList electives={filteredElectives} isFavorite={() => false} onToggleFavorite={() => {}} />
     </div>
   );
 }
